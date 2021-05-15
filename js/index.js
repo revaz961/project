d681e345eda8ab3c8d3f948d788423d45e383e52
revaz <revaz961@gmail.com>
2021-05-15 22:11:40 +0400

    first day

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed:5000,
+        dots: true,
+        pauseOnHover:true
+    });
+  });
